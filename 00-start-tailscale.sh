5b0b58e2888325302107e890b892ef9c96712582
Pete Keen <pete@petekeen.net>
2024-04-20 15:41:15 -0400

    Move blog to home server

--- 00-start-tailscale.sh
+++ 00-start-tailscale.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 /usr/local/bin/tailscaled --state=mem: &
-/usr/local/bin/tailscale up --auth-key=${TS_OAUTH_CLIENT_SECRET}?ephemeral=true\&preauthorized=true --advertise-tags=tag:fly-proxy --hostname=${FLY_APP_NAME}-${FLY_REGION}-${HOSTNAME} --accept-dns=false
+/usr/local/bin/tailscale up --auth-key=${TS_OAUTH_CLIENT_SECRET}?ephemeral=true\&preauthorized=true --advertise-tags=tag:fly-proxy --hostname=${FLY_APP_NAME}-${FLY_REGION}-${HOSTNAME}
